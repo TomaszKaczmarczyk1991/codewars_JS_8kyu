6a41b60d0463c054ac4dcbd54c5f5212d518f2df
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-07-20 14:12:53 +0200

    add arr2bin

---
+++ argumentsToBinaryAddition.js
@@ -0,0 +1,7 @@
+function arr2bin(arr){
+    let number = 0;
+        for(let i = 0; i < arr.length; i++) {
+            if(typeof arr[i] === 'number') number += arr[i];
+        }
+    return number.toString(2)
+  }
