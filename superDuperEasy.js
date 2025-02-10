b9373306a40f9f5a2a938b16c290de0e21bedc45
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2025-02-10 23:30:06 +0100

    Update superDuperEasy.js

--- superDuperEasy.js
+++ superDuperEasy.js
@@ -4,3 +4,8 @@ function problem(x){
     if(typeof x === 'string') return "Error";
     return (x * 50) + 6;
   }
+
+// shorter:
+const problem = x => typeof x === "string" ? "Error" : x * 50 + 6;
+
+console.log(problem(100)); // 5006
