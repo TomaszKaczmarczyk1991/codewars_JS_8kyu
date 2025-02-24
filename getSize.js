d6720322e1e5bbc557b1ab34bf9d1398da73b458
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-02-24 16:44:17 +0100

    add getSize.js

---
+++ getSize.js
@@ -0,0 +1,7 @@
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(width, height, depth) {
+  return [2 * (depth * width + width * height + height * depth), width * height * depth];
+}
+
+console.log(getSize(4, 2, 6));
