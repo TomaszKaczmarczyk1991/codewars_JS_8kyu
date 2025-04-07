7b167edbecc2ce32f62804dc0c30112fd9454e09
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-04-07 16:22:52 +0200

    add better solution

--- addLength.js
+++ addLength.js
@@ -2,3 +2,7 @@ function addLength(str) {
     const arr = str.split(' ');
     return arr.map(word => word + ' ' + word.length)
 }   
+
+// function addLength(str){
+//   return str.split(" ").map(s => `${s} ${s.length}`)
+// }
