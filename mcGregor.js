1d6ca353344b70311f619bb3a06cdda5193a64e9
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-04-17 04:43:10 +0200

    add mcGregor.js

---
+++ mcGregor.js
@@ -0,0 +1,4 @@
+function quote(fighter) {
+    if(fighter.toLowerCase() === 'george saint pierre') return "I am not impressed by your performance.";
+    if(fighter.toLowerCase() === 'conor mcgregor') return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+  };
