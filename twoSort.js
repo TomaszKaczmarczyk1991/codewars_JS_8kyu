36c552e2fd65210118b9dc7644b601dbb58abeab
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2025-05-13 18:18:30 +0200

    add better solution

--- twoSort.js
+++ twoSort.js
@@ -1,6 +1,3 @@
 function twoSort(s) {
-  const str = s.sort();
-  const result = [...str[0]].map(x => x + '***').join('').slice(0, -3);
-  
-  return result;
+  return s.sort()[0].split('').join('***');
 }
